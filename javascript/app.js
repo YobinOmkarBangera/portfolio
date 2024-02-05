58282caee1f31cbafab599dd5d8b427a8b9debae
YobinOmkarBangera <yobinbangera350@gmail.com>
2024-02-05 18:24:33 +0530

    updated shut option

--- javascript/app.js
+++ javascript/app.js
@@ -9,12 +9,17 @@ $(document).ready(function(){
     let hamburger = document.querySelector('.hamburger');
     let times = document.querySelector('.times');
     let mobilenav = document.querySelector('.mobile-nav');
+    let shut = document.querySelector('.shut');
    
     hamburger.addEventListener('click', function(){
         mobilenav.classList.add('open');
     });
 
     times.addEventListener('click', function(){
+        mobilenav.classList.remove('open');
+    });
+
+    shut.addEventListener('click', function(){
         mobilenav.classList.remove('open');
     });
 
